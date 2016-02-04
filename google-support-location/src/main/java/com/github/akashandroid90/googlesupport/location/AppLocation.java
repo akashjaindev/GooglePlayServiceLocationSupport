4726a3725c6fd6aa1b854dc82c677636ad1132ef
Akash Jain <akash.android90@gmail.com>
2016-02-04 10:24:50 +0530

    remove dir

--- google-support-location/src/main/java/com/github/akashandroid90/googlesupport/location/AppLocation.java
+++
@@ -1,8 +0,0 @@
-package com.github.akashandroid90.googlesupport.location;
-
-/**
- * Created by Akash Jain on 08-Dec-15.
- */
-public class AppLocation {
-    public static final String REQUEST_UPDATES = "request_updates";
-}
